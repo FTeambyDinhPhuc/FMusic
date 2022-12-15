2a9ea436cdfd65e882866a92c3b590fab2d3896a
Đình Phúc <38375962+dinhphuc1309@users.noreply.github.com>
2022-12-15 15:08:35 +0700

    chỉnh sửa layout

--- app/src/main/java/com/example/fmusic/MainActivity.kt
+++ app/src/main/java/com/example/fmusic/MainActivity.kt
@@ -4,21 +4,20 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
 import android.widget.ImageView
 import android.widget.LinearLayout
-import com.example.fmusic.fragments.HomeFragment
-import com.example.fmusic.fragments.LibraryFragment
-import com.example.fmusic.fragments.SearchFragment
+import com.example.fmusic.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var homeLayout: LinearLayout
+    private lateinit var musicLayout: LinearLayout
+    private lateinit var radioLayout: LinearLayout
     private lateinit var searchLayout: LinearLayout
     private lateinit var libraryLayout: LinearLayout
 
-    private lateinit var homeImage: ImageView
+    private lateinit var musicImage: ImageView
+    private lateinit var radioImage: ImageView
     private lateinit var searchImage: ImageView
     private lateinit var libraryImage: ImageView
 
@@ -30,11 +29,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun AnhXaView(){
-        homeLayout = findViewById<LinearLayout>(R.id.homeLayout)
+        musicLayout = findViewById<LinearLayout>(R.id.musicLayout)
+        radioLayout = findViewById<LinearLayout>(R.id.radioLayout)
         searchLayout = findViewById<LinearLayout>(R.id.searchLayout)
         libraryLayout = findViewById<LinearLayout>(R.id.libraryLayout)
 
-        homeImage = findViewById<ImageView>(R.id.homeImage)
+        musicImage = findViewById<ImageView>(R.id.musicImage)
+        radioImage = findViewById<ImageView>(R.id.radioImage)
         searchImage = findViewById<ImageView>(R.id.searchImage)
         libraryImage = findViewById<ImageView>(R.id.libraryImage)
     }
@@ -45,45 +46,80 @@ class MainActivity : AppCompatActivity() {
         //set fragment mặc định hiển thị
         supportFragmentManager.beginTransaction()
             .setReorderingAllowed(true)
-            .replace(R.id.fragmentContainer, HomeFragment())
+            .replace(R.id.fragmentContainer, MusicFragment())
             .commit()
 
         var nhanChuyenManHinh: View.OnClickListener? = null
         nhanChuyenManHinh = View.OnClickListener{
-            if(it == homeLayout)
+            if(it == musicLayout)
             {
                 if(selectedTab != 1)
                 {
                     //set fragment
                     supportFragmentManager.beginTransaction()
                         .setReorderingAllowed(true)
-                        .replace(R.id.fragmentContainer, HomeFragment())
+                        .replace(R.id.fragmentContainer, MusicFragment())
                         .commit()
 
                     //Xử lý các nút không được chọn
-                    searchImage.setImageResource(R.drawable.ic_search_24)
-                    libraryImage.setImageResource(R.drawable.ic_library_music_24)
+                    radioImage.setImageResource(R.drawable.ic_radio)
+                    searchImage.setImageResource(R.drawable.ic_search)
+                    libraryImage.setImageResource(R.drawable.ic_library_music)
 
+                    radioImage.setBackgroundColor(Color.TRANSPARENT)
                     searchImage.setBackgroundColor(Color.TRANSPARENT)
                     libraryImage.setBackgroundColor(Color.TRANSPARENT)
 
                     //Xử lý nút được chọn
-                    homeImage.setImageResource(R.drawable.ic_home_selected_24)
-                    homeImage.setBackgroundResource(R.drawable.round_back_icon_selected)
+                    musicImage.setImageResource(R.drawable.ic_album_selected)
+                    musicImage.setBackgroundResource(R.drawable.round_back_icon_selected)
 
                     //Xử lý animation
-                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
-                    scaleAnimation.setDuration(200)
-                    scaleAnimation.setFillAfter(true)
-                    homeLayout.startAnimation(scaleAnimation)
+//                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
+//                    scaleAnimation.setDuration(200)
+//                    scaleAnimation.setFillAfter(true)
+//                    homeLayout.startAnimation(scaleAnimation)
 
                     //set selectedTab
                     selectedTab = 1
                 }
             }
-            else if(it == searchLayout)
+            else if(it == radioLayout)
             {
                 if(selectedTab != 2)
+                {
+                    //set fragment
+                    supportFragmentManager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.fragmentContainer, RadioFragment())
+                        .commit()
+
+                    //Xử lý các nút không được chọn
+                    musicImage.setImageResource(R.drawable.ic_album)
+                    searchImage.setImageResource(R.drawable.ic_search)
+                    libraryImage.setImageResource(R.drawable.ic_library_music)
+
+                    musicImage.setBackgroundColor(Color.TRANSPARENT)
+                    searchImage.setBackgroundColor(Color.TRANSPARENT)
+                    libraryImage.setBackgroundColor(Color.TRANSPARENT)
+
+                    //Xử lý nút được chọn
+                    radioImage.setImageResource(R.drawable.ic_radio_selected)
+                    radioImage.setBackgroundResource(R.drawable.round_back_icon_selected)
+
+                    //Xử lý animation
+//                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
+//                    scaleAnimation.setDuration(200)
+//                    scaleAnimation.setFillAfter(true)
+//                    homeLayout.startAnimation(scaleAnimation)
+
+                    //set selectedTab
+                    selectedTab = 2
+                }
+            }
+            else if(it == searchLayout)
+            {
+                if(selectedTab != 3)
                 {
                     //set fragment
                     supportFragmentManager.beginTransaction()
@@ -92,29 +128,31 @@ class MainActivity : AppCompatActivity() {
                         .commit()
 
                     //Xử lý các nút không được chọn
-                    homeImage.setImageResource(R.drawable.ic_home_24)
-                    libraryImage.setImageResource(R.drawable.ic_library_music_24)
+                    musicImage.setImageResource(R.drawable.ic_album)
+                    radioImage.setImageResource(R.drawable.ic_radio)
+                    libraryImage.setImageResource(R.drawable.ic_library_music)
 
-                    homeImage.setBackgroundColor(Color.TRANSPARENT)
+                    musicImage.setBackgroundColor(Color.TRANSPARENT)
+                    radioImage.setBackgroundColor(Color.TRANSPARENT)
                     libraryImage.setBackgroundColor(Color.TRANSPARENT)
 
                     //Xử lý nút được chọn
-                    searchImage.setImageResource(R.drawable.ic_search_selected_24)
+                    searchImage.setImageResource(R.drawable.ic_search_selected)
                     searchImage.setBackgroundResource(R.drawable.round_back_icon_selected)
 
                     //Xử lý animation
-                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
-                    scaleAnimation.setDuration(200)
-                    scaleAnimation.setFillAfter(true)
-                    searchLayout.startAnimation(scaleAnimation)
+//                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
+//                    scaleAnimation.setDuration(200)
+//                    scaleAnimation.setFillAfter(true)
+//                    searchLayout.startAnimation(scaleAnimation)
 
                     //set selectedTab
-                    selectedTab = 2
+                    selectedTab = 3
                 }
             }
             else if(it == libraryLayout)
             {
-                if(selectedTab != 3)
+                if(selectedTab != 4)
                 {
                     //set fragment
                     supportFragmentManager.beginTransaction()
@@ -123,29 +161,32 @@ class MainActivity : AppCompatActivity() {
                         .commit()
 
                     //Xử lý các nút không được chọn
-                    homeImage.setImageResource(R.drawable.ic_home_24)
-                    searchImage.setImageResource(R.drawable.ic_search_24)
+                    musicImage.setImageResource(R.drawable.ic_album)
+                    radioImage.setImageResource(R.drawable.ic_radio)
+                    searchImage.setImageResource(R.drawable.ic_search)
 
-                    homeImage.setBackgroundColor(Color.TRANSPARENT)
+                    musicImage.setBackgroundColor(Color.TRANSPARENT)
+                    radioImage.setBackgroundColor(Color.TRANSPARENT)
                     searchImage.setBackgroundColor(Color.TRANSPARENT)
 
                     //Xử lý nút được chọn
-                    libraryImage.setImageResource(R.drawable.ic_library_music_selected_24)
+                    libraryImage.setImageResource(R.drawable.ic_library_music_selected)
                     libraryImage.setBackgroundResource(R.drawable.round_back_icon_selected)
 
                     //Xử lý animation
-                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
-                    scaleAnimation.setDuration(200)
-                    scaleAnimation.setFillAfter(true)
-                    libraryLayout.startAnimation(scaleAnimation)
+//                    val scaleAnimation = ScaleAnimation(1.0f, 1.0f, 0.8f, 1f)
+//                    scaleAnimation.setDuration(200)
+//                    scaleAnimation.setFillAfter(true)
+//                    libraryLayout.startAnimation(scaleAnimation)
 
                     //set selectedTab
-                    selectedTab = 3
+                    selectedTab = 4
                 }
             }
         }
 
-        homeLayout.setOnClickListener(nhanChuyenManHinh)
+        musicLayout.setOnClickListener(nhanChuyenManHinh)
+        radioLayout.setOnClickListener(nhanChuyenManHinh)
         searchLayout.setOnClickListener(nhanChuyenManHinh)
         libraryLayout.setOnClickListener(nhanChuyenManHinh)
 
--- app/src/main/java/com/example/fmusic/fragments/MusicFragment.kt
+++ app/src/main/java/com/example/fmusic/fragments/MusicFragment.kt
@@ -1,15 +1,11 @@
 package com.example.fmusic.fragments
 
-import android.app.PendingIntent.getService
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.fmusic.MainActivity
 import com.example.fmusic.R
 import com.example.fmusic.models.TaiXeModel
 import com.example.fmusic.service_api.APIService
@@ -18,40 +14,35 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
-class HomeFragment : Fragment() {
+class MusicFragment : Fragment() {
     private lateinit var mView: View
-    private lateinit var txtFmHome: TextView
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-    {
+    private lateinit var txtFmMusic: TextView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.fragment_home, container, false)
-        txtFmHome = mView.findViewById(R.id.txtFmHome)
+        mView = inflater.inflate(R.layout.fragment_music, container, false)
+        txtFmMusic = mView.findViewById(R.id.txtFmMusic)
         CallApi()
         return mView
     }
+
     private fun CallApi(){
-       val dataservice: Dataservice = APIService.getService
+        val dataservice: Dataservice = APIService.getService
         val retrofitData = dataservice.getTaiXe(1)
-        retrofitData.enqueue(object : Callback<TaiXeModel>{
+        retrofitData.enqueue(object : Callback<TaiXeModel> {
             override fun onResponse(call: Call<TaiXeModel>, response: Response<TaiXeModel>) {
                 val TaiXe = response.body()
                 if(TaiXe!= null){
-                    txtFmHome.setText(TaiXe.data.diachi)
+                    txtFmMusic.setText(TaiXe.data.diachi)
                 }else
                 {
-                    txtFmHome.setText("Khong co gi")
+                    txtFmMusic.setText("Khong co gi")
                 }
             }
-
-            override fun onFailure(call: Call<TaiXeModel>, t: Throwable) {
-
-
-            }
-
+            override fun onFailure(call: Call<TaiXeModel>, t: Throwable) {}
         })
     }
-
 
 }
